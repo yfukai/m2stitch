5e235ce8afe608b9d5565340ee6650e2f40867a1
Yohsuke Fukai <ysk@yfukai.net>
2022-02-11 07:39:13 +0900

    test passing

--- tests/test_stitching.py
+++ tests/test_stitching.py
@@ -21,9 +21,9 @@ def test_image_path(shared_datadir: str) -> Tuple[npt.NDArray, pd.DataFrame]:
 def test_stitching(test_image_path: Tuple[npt.NDArray, pd.DataFrame]) -> None:
     testimages, props = test_image_path
     """It exits with a status code of zero."""
-    rows = props["col"].to_list()
-    cols = props["row"].to_list()
-    result_df, _ = stitch_images(testimages, rows, cols)
+    rows = props["row"].to_list()
+    cols = props["col"].to_list()
+    result_df, _ = stitch_images(testimages, rows, cols, row_col_transpose=False)
     assert np.array_equal(result_df.index, props.index)
     assert np.max(np.abs(result_df["x_pos"] - props["x_pos"])) < 2
     assert np.max(np.abs(result_df["y_pos"] - props["y_pos"])) < 2
