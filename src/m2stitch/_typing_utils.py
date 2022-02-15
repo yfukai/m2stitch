2e99fcd0cd2a625a60be82a6454bb2006c18b4d8
Yohsuke Fukai <ysk@yfukai.net>
2022-02-15 09:41:36 +0900

    fixed bool datetype

--- src/m2stitch/_typing_utils.py
+++ src/m2stitch/_typing_utils.py
@@ -7,7 +7,7 @@ import numpy.typing as npt
 NumArray = npt.NDArray[Any]
 FloatArray = npt.NDArray[np.float_]
 IntArray = npt.NDArray[np.int_]
-BoolArray = npt.NDArray[np.bool]
+BoolArray = npt.NDArray[np.bool_]
 
 Int = Union[int, np.int_]
 Float = Union[float, np.float_]
