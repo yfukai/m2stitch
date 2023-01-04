70868af2772cc798f3fb874500e032401fa51c0d
Yohsuke Fukai <ysk@yfukai.net>
2023-01-04 21:22:01 +0900

    updating test

--- tests/test_stitching.py
+++ tests/test_stitching.py
@@ -18,6 +18,135 @@ def test_image_path(shared_datadir: str) -> Tuple[npt.NDArray, pd.DataFrame]:
     return (testimages, props)
 
 
+@pytest.fixture
+def test_image_path_mimuelle2212(
+    shared_datadir: str,
+) -> Tuple[npt.NDArray, pd.DataFrame]:
+    testimages = np.load(path.join(shared_datadir, "images_mimuelle2212.npy"))
+    props = pd.DataFrame(
+        {
+            "col": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1,
+                0,
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1,
+                0,
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1,
+                0,
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1,
+                0,
+            ],
+            "row": [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                4,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                6,
+                6,
+                6,
+                6,
+                6,
+                6,
+                6,
+                7,
+                7,
+                7,
+                7,
+                7,
+                7,
+                7,
+            ],
+        }
+    )
+    assert np.array_equal(props.index, np.arange(testimages.shape[0]))
+    return (testimages, props)
+
+
 def test_stitching(test_image_path: Tuple[npt.NDArray, pd.DataFrame]) -> None:
     testimages, props = test_image_path
     """It exits with a status code of zero."""
@@ -27,6 +156,16 @@ def test_stitching(test_image_path: Tuple[npt.NDArray, pd.DataFrame]) -> None:
     assert np.array_equal(result_df.index, props.index)
     assert all(np.abs(result_df["y_pos"] - props["y_pos"]) <= props["allowed_error"])
     assert all(np.abs(result_df["x_pos"] - props["x_pos"]) <= props["allowed_error"])
+
+
+def test_stitching_mimuelle2212(
+    test_image_path_mimuelle2212: Tuple[npt.NDArray, pd.DataFrame]
+) -> None:
+    testimages, props = test_image_path_mimuelle2212
+    """It exits with a status code of zero."""
+    cols = props["col"].to_list()
+    rows = props["row"].to_list()
+    result_df, _ = stitch_images(testimages, rows, cols, row_col_transpose=False)
 
 
 def test_stitching_init_guess(
